243db3c388b63a0f6df83452c2ca8f0f97f0c368
Elias Merabet Ponton <elias.merabet.p@gmail.com>
2017-10-11 20:39:07 +0200

    Update RouteConfig.cs

--- MVC/Tuto3/RouteConfig.cs
+++ MVC/Tuto3/RouteConfig.cs
@@ -1,18 +1,21 @@
- public static void RegisterRoutes(RouteCollection routes)
+public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Custom", "Custom/List/",
+            routes.MapRoute(
+                "JS",
+                "JS/code={code}",
                 new
                 {
                     Controller = "Home",
-                    Action = "List"
-                });
+                    Action = "Selected",
+                    code = 0
+                }, new {code = @"\d{1,2}" }
+                );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
         }
